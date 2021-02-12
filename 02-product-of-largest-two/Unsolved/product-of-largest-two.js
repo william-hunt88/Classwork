cbc1fb78a5c3a29ad5d90a8f47b09b8d4651362b
w.hunt <wfh2d88@gmail.com>
2021-02-12 11:23:18 -0600

    for BCS

---
+++ 02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -0,0 +1,16 @@
+// Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
+
+// var productOfLargestTwo = function(arr) {
+//     // empty arry for largest #'s
+//     biggestTwo = []
+
+//     // Loops thru and push biggest two to array
+//     for(var i = 0; i < arr.length; i++){
+//         if(arr[i] > biggestTwo[0] || biggestTwo[1]){
+//             biggestTwo.push(arr[i])
+//         }else{
+//             return;
+//         }
+
+//     }
+// };
